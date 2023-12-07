c27d292f95b78ce198e1c134e45ba7d15ac16e2b
Julio Condor <julioc3bgud@gmail.com>
2023-12-07 16:08:03 -0500

    Agregando funcionalidad agregar productos con mong

--- pages/products.js
+++ pages/products.js
@@ -1,7 +1,16 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Products(){
+
+  useEffect(() => {
+    axios.get('/products').then(response =>{
+      console.log(response.data);
+    })
+  })
+
   return (
     <Layout>
       <Link className="bg-green-200 p-1 px-2 rounded-md" href={'/products/new'}>Agregar nuevo Producto</Link>
--- pages/products/new.js
+++ pages/products/new.js
@@ -1,14 +1,31 @@
 import Layout from "@/components/Layout";
 import Link from "next/link";
 import { useState } from "react";
-
+import axios from "axios";
+import { redirect } from "next/dist/server/api-utils";
+import { useRouter } from "next/router";
 export default function NewProducts() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
+  const [goToProducts, setGoToProducts] = useState(false);
+  const router = useRouter();
+  async function createProduct(ev){
+    ev.preventDefault();
+    const data = {title, description, price}
+    await axios.post('/api/products', data)
+    setGoToProducts(true)
+  }
+  if(goToProducts){
+    router.push('/products')
+  }
+  
 
   return (
     <Layout>
+      <form onSubmit={createProduct}>
+
+      
       <h1>Nuevo Producto</h1>
       <label>Nombre del producto</label>
       <input
@@ -18,10 +35,14 @@ export default function NewProducts() {
         onChange={ev => setTitle(ev.target.value)}
       ></input>
       <label>Descripción</label>
-      <textarea placeholder="Escribe la descripción de el producto aqui..."></textarea>
+      <textarea placeholder="Escribe la descripción de el producto aqui..."
+      value={description}
+      onChange={ev => setDescription(ev.target.value)}></textarea>
       <label>Precio USD</label>
-      <input type="number" placeholder="Ingresa el precio en USD"></input>
-      <button className="btn-primary">Guardar</button>
+      <input value={price}
+        onChange={ev => setPrice(ev.target.value)} type="number" placeholder="Ingresa el precio en USD"></input>
+      <button type="submit" className="btn-primary">Guardar</button>
+      </form>
     </Layout>
   );
 }
