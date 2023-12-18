ae8f5f27eaa003e7810eaca606109faa4d64016c
Julio Condor <julioc3bgud@gmail.com>
2023-12-18 12:32:38 -0500

    Agregando Ordenes

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,7 +1,9 @@
+import { Order } from "@/lib/models/Order";
 import { mongooseConnect } from "@/lib/mongoose";
 
 export default async function handler(req, res) {
   await mongooseConnect()
+
   res.json(await Order.find().sort({createdAt:-1}))
   
 }
--- pages/orders.js
+++ pages/orders.js
@@ -1,25 +1,56 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default function Orders(){
+export default function Orders() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/orders").then((response) => {
+      setOrders(response.data);
+    });
+  }, []);
+
   return (
     <Layout>
       <h1>Ordenes</h1>
       <table className="basic">
         <thead>
           <tr>
-            <th>
-              ID
-            </th>
-            <th>
-
-            </th>
-            <th>
-              
-            </th>
+            <th>ID</th>
+            <th>Pagado</th>
+            <th>Datos</th>
+            <th>Productos</th>
           </tr>
         </thead>
-
+        <tbody>
+          {orders.length > 0 &&
+            orders.map((order) => (
+              <tr>
+                <td>{new Date(order.createdAt).toLocaleString()}</td>
+                <td className={order.paid ? "text-green-600" : "text-red-600"}>
+                  {order.paid ? "YES" : "NO"}
+                </td>
+                <td>
+                  {order.name} {order.email}
+                  <br />
+                  {order.city} {order.postalCode} {order.country}
+                  <br />
+                  {order.streetAddress}
+                </td>
+                <td>
+                  {order.line_items.map((l) => (
+                    <>
+                      {l.price_data?.product_data.name}{" "}
+                      <label className="text-blue-600">x{l.quantity}</label>{" "}
+                      <br />
+                    </>
+                  ))}
+                </td>
+              </tr>
+            ))}
+        </tbody>
       </table>
     </Layout>
-  )
+  );
 }
