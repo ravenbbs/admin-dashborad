a7dffd5313188b408dc6c60c90bc5b9682738c31
Julio Condor <julioc3bgud@gmail.com>
2023-12-24 21:45:52 -0500

    Probando reparar el inicio de sesion erroneo

--- components/Layout.js
+++ components/Layout.js
@@ -14,7 +14,7 @@ const [showNav, setShowNav] = useState(false)
 
         <div className="text-center">
           <button
-            onClick={() => signIn("google")}
+            onClick={async() => await signIn("google")}
             className="bg-gray-300 border-gray-500 border-2 shadow-lg rounded-md px-4 py-2 font-semibold "
           >
             Ingresa Con Google
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import HomeHeader from "@/components/HomeHeader";
 import HomeStats from "@/components/HomeStats";
 import Layout from "@/components/Layout";
-import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
 
   return (
     <Layout>
