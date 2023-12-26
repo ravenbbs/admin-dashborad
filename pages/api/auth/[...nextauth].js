28be7afe919460ef46b52f5e65f70956d47b0f50
Julio Condor <julioc3bgud@gmail.com>
2023-12-25 20:42:27 -0500

    prueba sobre el inicio de sesion, creo funcional

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -14,8 +14,15 @@ export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    }),
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    })
   ],
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
